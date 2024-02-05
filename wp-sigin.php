6232fee40e29e95e2b5a5f1c1d0c8e2a602c3486
esteplogic <info@esteplogic.com>
2024-02-05 17:30:10 +0530

    created signin module using wp-signin.php

--- wp-sigin.php
+++ wp-sigin.php
@@ -1,2 +1,7 @@
 wp sign-in file is used as one way communication to initiate the login process of main admin.
 we can write this file according to the way of simuntaneously in the place of many access areas.
+
+
+
+ajf kasjfgkol ejufgoierjuo9fgeprgerpgi ajsfdlkjsafjsfjop9uj 
+
