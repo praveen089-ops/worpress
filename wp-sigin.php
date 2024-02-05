d6091cd7fe449365fad864c2d13fd3991f2a5fab
praveen089-ops <praveenkumar.pk409621@gmail.com>
2024-02-05 18:44:34 +0530

    Revert "Please ignore previous commit"
    This reverts commit 6232fee40e29e95e2b5a5f1c1d0c8e2a602c3486.

--- wp-sigin.php
+++ wp-sigin.php
@@ -1,7 +1,2 @@
 wp sign-in file is used as one way communication to initiate the login process of main admin.
 we can write this file according to the way of simuntaneously in the place of many access areas.
-
-
-
-ajf kasjfgkol ejufgoierjuo9fgeprgerpgi ajsfdlkjsafjsfjop9uj 
-
